28f58986476775dbe8878891df9a3363d8aca851
Abdul Haseeb <34100036+tigerdrifter@users.noreply.github.com>
2019-10-10 01:12:48 +0500

    Add files via upload

---
+++ Satoshi.go
@@ -0,0 +1,60 @@
+package assignment02IBC
+
+import (
+	"encoding/gob"
+	"log"
+	"math/rand"
+	"net"
+
+	a1 "github.com/tigerdrifter/assignment02IBC"
+)
+
+func SendBlockchain(port net.Conn, chainHead *a1.Block) {
+	gobEncoder := gob.NewEncoder(port)
+	err := gobEncoder.Encode(&chainHead)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func SendPeers(port net.Conn, peers []string) {
+	gobEncoder := gob.NewEncoder(port)
+	err := gobEncoder.Encode(peers)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func SendTrans(trans string, miner string) {
+	conn, err := net.Dial("tcp", miner)
+	if err != nil {
+		log.Println(err)
+	}
+	println("\nMiner: ", miner)
+	trans1 := "Transaction"
+
+	gobEncoder := gob.NewEncoder(conn)
+	err = gobEncoder.Encode(&trans1)
+
+	gobEncoder = gob.NewEncoder(conn)
+	err = gobEncoder.Encode(&trans)
+
+	println("\nTransaction Sent")
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	conn.Close()
+}
+
+func SelectMiner(nodes int, peersPort []string, port string) string {
+	miner := 0
+	for {
+		miner = rand.Intn(nodes)
+		if port != peersPort[miner] {
+			break
+		}
+	}
+	return peersPort[miner]
+}
